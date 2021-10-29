a5f06477acaa70e84664e0a901fb020c0443eb5c
Bryan <byranzsilva@hotmail.com>
2021-10-29 12:37:30 -0500

    Se añade un formulario de registro y el procesamiento del mismo

--- blog_viajes.js
+++ blog_viajes.js
@@ -30,6 +30,48 @@ app.get('/', (req, res)=>{
     })
 })
 
+app.get('/registro', (req, res)=>{
+    res.render('registro', {mensaje: req.flash('mensaje')})
+})
+
+app.post('/procesar_registro', (req, res)=>{
+    pool.getConnection((err,connection)=>{
+        const email = req.body.email.toLowerCase().trim()
+        const password = req.body.contrasena
+        const pseudonimo = req.body.pseudonimo.trim()
+
+        const queryEmail = `SELECT * FROM autores WHERE email = ${connection.escape(email)}`
+
+        connection.query(queryEmail, (error,filas,campos)=>{
+            if(filas.length > 0){
+                req.flash('mensaje', 'Este email ya se encuentra en uso por otro usuario')
+                res.redirect('/registro')
+            }else{
+                const queryPseudonimo = `SELECT * FROM autores WHERE pseudonimo = ${connection.escape(pseudonimo)}`
+
+                connection.query(queryPseudonimo, (error,filas,campos)=>{
+                    if(filas.length > 0){
+                        req.flash('mensaje', 'Este pseudónimo ya se encuentra en uso, elige otro')
+                        res.redirect('/registro')
+                    }else{
+                        const query = `INSERT INTO autores 
+                        (email, contrasena, pseudonimo) VALUES (
+                            ${connection.escape(email)}, 
+                            ${connection.escape(password)}, 
+                            ${connection.escape(pseudonimo)})`
+
+                        connection.query(query, (error,filas,campos)=>{
+                            req.flash('mensaje', 'Se ha registrado correctamente')
+                            res.redirect('/registro')
+                        })
+                    }
+                })
+            }
+        })
+        connection.release()
+    })
+})
+
 app.listen(8080, ()=>{
     console.log("Servidor iniciado")
 })
